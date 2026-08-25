74f54e1969b6844e766ae954f47334892da5a397
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-08-25 01:44:53 +0000

    Sync LeetCode submission - Sum of Squares of Special Elements  (java)

---
+++ solutions/problems/sum_of_squares_of_special_elements_/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int sumOfSquares(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+          if(nums.length%(i+1)!=0){
+            continue;
+          }
+          sum+=Math.pow(nums[i],2);
+        }
+        return sum;
+    }
+}
