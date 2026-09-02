f69270d8a24b9860d085f1c0c972f94c421167b5
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-09-02 04:08:43 +0000

    Sync LeetCode submission - Construct Uniform Parity Array I (java)

---
+++ solutions/problems/construct_uniform_parity_array_i/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // case 1 all are even or all are odd
+        // case 2 atleast one odd , can be reduced to odd 
+
+        return true;
+    }
+}
