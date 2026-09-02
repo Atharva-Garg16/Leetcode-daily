b1c01a71b895a04010613eb13bd7f4eccdb11b9e
Atharva-Garg16 <atharvagarg1610@gmail.com>
2026-09-02 17:15:28 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- solutions/problems/contains_duplicate/solution.java
+++ solutions/problems/contains_duplicate/solution.java
@@ -1,17 +1,24 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         // alternate solution sort and than check
-        boolean b=false;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-            if(hm.get(nums[i])>1){
-             b=true;
-             break;
-            }
+        // brute o(n^2) sort o(n) this O(n) use of hashset O(n)
+        // boolean b=false;
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        //     if(hm.get(nums[i])>1){
+        //      b=true;
+        //      break;
+        //     }
             
+        // }
+        // return b;
+        HashSet<Integer> hs =new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            hs.add(nums[i]);
         }
-        return b;
+        return !(hs.size()==nums.length);
+        
        
     }
 }
